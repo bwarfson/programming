5b4f921f4504b07203b97b8558f0db5cb93ae193
Brad Wight <bwight@scentsy.com>
2019-04-23 13:40:58 -0600

    OOP School Example

---
+++ dotnet/oop/school/SchoolClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace school
+{
+    class SchoolClass
+    {
+    }
+}
---
+++ dotnet/oop/school/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace school
+{
+    public class Student : Person
+    {
+        public Guid StudentNumber { get; set; }
+    }
+}
